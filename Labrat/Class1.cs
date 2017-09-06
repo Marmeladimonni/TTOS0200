baf981de33f3a9371d2b3d1841fe416fe7925e09
Teemu Mäkinen <K2001@student.jamk.fi>
2017-09-06 11:02:47 +0300

    Added Template for tasks

---
+++ Labrat/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labrat
+{
+    class Lab01
+    {
+        public static void Task1()
+        {
+            //asdf
+            System.Console.WriteLine("WIP...");
+        }
+    }
+}
