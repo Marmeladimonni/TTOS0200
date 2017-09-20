1be6faa197d2c0a2638cab5b0eaee57c2ba057e2
Teemu Mäkinen <K2001@student.jamk.fi>
2017-09-20 11:11:47 +0300

    Added Lab3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,17 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Labrat
+namespace ConsoleApp1
 {
     class Program
     {
-        public string syote;
         static void Main(string[] args)
         {
-            Console.Write("Anna luku: ");
-            Lab3.OnkoLuku(syote);
-
-            //Lab3.OnkoPvm();
         }
     }
 }
---
+++ Labrat/Lab3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labrat
+{
+    class Lab3
+    {
+        
+        public static bool OnkoLuku(string syote)
+        {
+            
+            if 
+            {
+
+            }
+            
+            return false;
+        }
+
+        public static bool OnkoPvm(string syote)
+        {
+
+
+            return false;
+        }
+    }
+}
