fb69375d894abf1d8025b38c3878d1ff8c1bab1e
Teemu Mäkinen <K2001@student.jamk.fi>
2017-09-27 11:02:16 +0300

    Added a new class (Lab5)

---
+++ Labrat/Lab5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labrat
+{
+    class Lab5
+    {
+        public static void Teht4()
+        {
+           
+        }
+
+        public class Employee
+        {
+            public string Name { get; set; }
+            public string Profession { get; set; }
+            public double Salary { get; set; }
+
+            void PrintEmployee(string Name, string Profession, double Salary)
+            {
+                Name = "Kirsi Kernel";
+                Profession = "Teacher";
+                Salary = 1200;
+            }
+        }
+
+        public class Boss : Employee
+        {
+            public string Car { get; set; }
+            public double Bonus { get; set; }
+
+            void PrintBoss()
+            {
+                Name = "Jussi Jurkka";
+                Profession = "Head of Institute";
+                Salary = 9000;
+                Car = "Audi";
+                Bonus = 5000;
+            }
+        }
+    }
+}
