bd8c52b9aebd257b6d4528d62c007daafe73481c
Teemu Mäkinen <K2001@student.jamk.fi>
2017-11-01 10:08:34 +0200

    Added VKO44

---
+++ KOT/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KOT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //KÄYTÄ AINA TRY-CATCH
+            try
+            {
+                string nimi;
+                string filupath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string filu = filupath + @"\testi.txt";
+                //Kirjoitetaan käyttäjän antamat rivit tiedostoon
+                //Luodaan (StreamWriter-) olio, johon kirjoitetaan
+                StreamWriter sw = new StreamWriter(filu);
+
+                do
+                {
+                    Console.Write("Anna nimi (Enter lopettaa):");
+                    nimi = Console.ReadLine();
+                    if (nimi.Length > 0)
+                        sw.WriteLine(nimi);
+                } while (nimi.Length != 0);
+                sw.Close();
+                if (File.Exists(filu))
+                {
+                    //Avataan tiedosto uudestaan ja luetaan se. Näytetään konsolissa.
+                    string teksti = File.ReadAllText(filu);
+                    Console.WriteLine(teksti);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Tiedostoa ei löydy!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ VK44/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public struct Ohjastaja
+    {
+        public string Nimi;
+        public int Startit;
+        public int Voitot;
+        public float VoittoPros;
+    }
+    class Program
+    {
+        static void LueOhjastajatVer2()
+        {
+            try
+            {
+                //Luetaan CSV-tiedostosta tiedot ja tallennetaan structeihin
+                string erotin = ";";
+                //Luetaan kaikki rivit muuttujaan
+                string[] rivit = System.IO.File.ReadAllLines(@"d:\K2001\tilasto2017.csv");
+                Ohjastaja kuski;
+                int lkm = rivit.Length;
+                Console.WriteLine("Ohjastajia yhteensä {0}", lkm -1);
+                //Käydään muistiin luetut rivit läpi
+                for (int i = 1; i < lkm; i++)
+                {
+                    string[] sanat = rivit[i].Split(erotin.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    //Tietueita on kahdenlaisia: V1: Etunimi 
+                    if (int.TryParse(sanat[2], out int apu))
+                    {
+                        kuski.Nimi = sanat[0] + " " + sanat[1];
+                        kuski.Startit = int.Parse(sanat[2]);
+                        kuski.Voitot = int.Parse(sanat[3]);
+                        kuski.VoittoPros = (100F * kuski.Voitot / kuski.Startit);
+                    }
+                    else
+                    {
+                        kuski.Nimi = sanat[0] + " " + sanat[1] + " " + sanat[2];
+                        kuski.Startit = int.Parse(sanat[3]);
+                        kuski.Voitot = int.Parse(sanat[4]);
+                        kuski.VoittoPros = (100F * kuski.Voitot / kuski.Startit);
+                    }
+                    
+
+                    //näytetään tulos
+                    Console.WriteLine("{0}: {1} Startit {2} Voitot {3} Voittoprosentti {4}", i, kuski.Nimi, kuski.Startit, kuski.Voitot, kuski.VoittoPros);
+                }
+                Console.WriteLine("That's all folks!");
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        static void LueOhjastajatVer1()
+        {
+            try
+            {
+                //Luetaan tiedosto läpi rivi kerrallaan
+                int counter = 0;
+                string line;
+                System.IO.StreamReader stream = new System.IO.StreamReader(@"d:\K2001\tilasto2017.txt");
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine(counter + ":" + line);
+                    counter++;
+                }
+                stream.Close();     //suljetaan striimi niin tiedosto ei jää "roikkumaan"
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                LueOhjastajatVer2();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
